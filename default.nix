7c7534a5cac1ef3c8ca707de3218b490af6f87e3
Andreas Källberg <anka.213@gmail.com>
2023-08-14 14:41:46 +0300

    Don't  build the gf server
    This prevents needing the broken cgi package

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ system ? builtins.currentSystem, compiler ? "ghc8104" }:
+{ system ? builtins.currentSystem, compiler ? "ghc92" }:
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { inherit system; };
@@ -12,20 +12,22 @@ let
           "gf-lsp"
           (gitignore ./.)
           { };
-      gf = pkgs.haskell.lib.overrideCabal (hself.callCabal2nix "gf" sources.gf-core { }) (
-        _old: {
-          # Fix utf8 encoding problems
-          patches = [
-            (
-              pkgs.fetchpatch {
-                url = "https://github.com/anka-213/gf-core/commit/6f1ca05fddbcbc860898ddf10a557b513dfafc18.patch";
-                sha256 = "17vn3hncxm1dwbgpfmrl6gk6wljz3r28j191lpv5zx741pmzgbnm";
-              }
-            )
-            ./nix/expose-all.patch
-          ];
-        }
-      );
+      gf = pkgs.haskell.lib.overrideCabal
+        (hself.callCabal2nixWithOptions "gf" sources.gf-core "--flag=-server" { })
+        (
+          _old: {
+            # Fix utf8 encoding problems
+            patches = [
+              (
+                pkgs.fetchpatch {
+                  url = "https://github.com/anka-213/gf-core/commit/6f1ca05fddbcbc860898ddf10a557b513dfafc18.patch";
+                  sha256 = "17vn3hncxm1dwbgpfmrl6gk6wljz3r28j191lpv5zx741pmzgbnm";
+                }
+              )
+              ./nix/expose-all.patch
+            ];
+          }
+        );
     };
   };
 
